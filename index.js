b756970e242653282967fe1111b8feeecb8f84c5
David Nam <knam13@my.bcit.ca>
2022-03-26 21:20:28 -0700

    Dinuja.css

--- index.js
+++ index.js
@@ -51,14 +51,24 @@ app.get("/test", function(req, res) {
   res.send(fs.readFileSync("./app/html/test.html", "utf8"));
 });
 
-app.get("/login", function(req, res) {
+app.get("/account", function(req, res) {
   res.setHeader("Content-Type", "text/html");
-  res.send(fs.readFileSync("./app/html/login.html", "utf8"));
+  res.send(fs.readFileSync("./app/html/account.html", "utf8"));
 });
 
-app.get("/signup", function(req, res) {
+app.get("/editProfile", function(req, res) {
   res.setHeader("Content-Type", "text/html");
-  res.send(fs.readFileSync("./app/html/signup.html", "utf8"));
+  res.send(fs.readFileSync("./app/html/editProfile.html", "utf8"));
+});
+
+app.get("/request", function(req, res) {
+  res.setHeader("Content-Type", "text/html");
+  res.send(fs.readFileSync("./app/html/request.html", "utf8"));
+});
+
+app.get("/requestSuccess", function(req, res) {
+  res.setHeader("Content-Type", "text/html");
+  res.send(fs.readFileSync("./app/html/requestSuccess.html", "utf8"));
 });
 
 // RUN SERVER
