70d109569a08bdffae98b49068a8114a86a23f05
David Nam <knam13@my.bcit.ca>
2022-04-06 14:36:13 -0700

    Revert "Commit."
    This reverts commit deef17ca650af663c09ab4c5c72d14069c50f8ce.

--- index.js
+++ index.js
@@ -3,26 +3,18 @@
 
 
 // REQUIRES
-import express from "express";
+const express = require("express");
 const app = express();
 app.use(express.json());
-import fs from "fs";
-import got from "got";
-import { JSDOM } from "jsdom";
-
+const fs = require("fs");
 
 app.use("/js", express.static("./public/js"));
 app.use("/css", express.static("./public/css"));
 app.use("/img", express.static("./public/img"));
 app.get("/", function(req, res) {
-  const URL = "https://storage.cloud.google.com/taste-of-home/public/html/main.html";
-  got(URL).then(response => {
-    const dom = new JSDOM(response.body);
-    console.log(dom.window.document.querySelector("title").textContent);
-    res.send(dom.serialize());
-  }).catch(err => {
-    console.log(err);
-  })
+
+  const doc = fs.readFileSync("./public/html/main.html", "utf8");
+  res.send(doc);
 });
 
 app.get("/search", function(req, res) {
