fcdae8ad2ec23df2dc85309af244460c1d8731b2
David Nam <knam13@my.bcit.ca>
2022-04-06 12:08:46 -0700

    asdasd

--- functions/index.js
+++ functions/index.js
@@ -11,36 +11,32 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const cors = require("cors");
 
-app.use("/js", express.static("./public/js"));
-app.use("/css", express.static("./public/css"));
-app.use("/img", express.static("./public/img"));
-
 app.get("/", function(req, res) {
-  const doc = fs.readFileSync("./public/html/main.html", "utf8");
+  const doc = fs.readFileSync("https://storage.cloud.google.com/taste-of-home/public/html/main.html", "utf8");
   res.send(doc);
 });
 
 app.get("/search", function(req, res) {
-  const doc = fs.readFileSync("./public/html/search.html", "utf8");
+  const doc = fs.readFileSync("https://storage.cloud.google.com/taste-of-home/public/html/search.html", "utf8");
   res.send(doc);
 });
 
 app.get("/nav", function(req, res) {
   cors()(req, res, () => {
     res.setHeader("Content-Type", "text/html");
-    const doc = fs.readFileSync("./public/data/footer.html", "utf8");
+    const doc = fs.readFileSync("https://storage.cloud.google.com/taste-of-home/public/html/footer.html", "utf8");
     res.send(doc);
   });
 });
 
 app.get("/login", function(req, res) {
   res.setHeader("Content-Type", "text/html");
-  res.send(fs.readFileSync("./public/html/login.html", "utf8"));
+  res.send(fs.readFileSync("https://storage.cloud.google.com/taste-of-home/public/html/login.html", "utf8"));
 });
 
 app.get("/signup", function(req, res) {
   res.setHeader("Content-Type", "text/html");
-  res.send(fs.readFileSync("./public/html/signup.html", "utf8"));
+  res.send(fs.readFileSync("https://storage.cloud.google.com/taste-of-home/public/html/signup.html", "utf8"));
 });
 
 app.get("/new", function(req, res) {
@@ -48,10 +44,10 @@ app.get("/new", function(req, res) {
 
   if (formatOfResponse == "countries") {
     res.setHeader("Content-Type", "text/html");
-    res.send(fs.readFileSync("./public/data/countries-overlay.html", "utf8"));
+    res.send(fs.readFileSync("https://storage.cloud.google.com/taste-of-home/public/data/countries-overlay.html", "utf8"));
   } else if (formatOfResponse == "snacks") {
     res.setHeader("Content-Type", "text/html");
-    res.send(fs.readFileSync("./public/data/snacks-overlay.html", "utf8"));
+    res.send(fs.readFileSync("https://storage.cloud.google.com/taste-of-home/public/data/snacks-overlay.html", "utf8"));
   } else {
     res.send({ status: "fail", msg: "Wrong format!" });
   }
@@ -59,33 +55,33 @@ app.get("/new", function(req, res) {
 
 app.get("/card", function(req, res) {
   res.setHeader("Content-Type", "text/html");
-  res.send(fs.readFileSync("./public/html/card.html", "utf8"));
+  res.send(fs.readFileSync("https://storage.cloud.google.com/taste-of-home/public/html/card.html", "utf8"));
 });
 
 app.get("/account", function(req, res) {
   res.setHeader("Content-Type", "text/html");
-  res.send(fs.readFileSync("./public/html/account.html", "utf8"));
+  res.send(fs.readFileSync("https://storage.cloud.google.com/taste-of-home/public/html/account.html", "utf8"));
 });
 
 app.get("/editProfile", function(req, res) {
   res.setHeader("Content-Type", "text/html");
-  res.send(fs.readFileSync("./public/html/editProfile.html", "utf8"));
+  res.send(fs.readFileSync("https://storage.cloud.google.com/taste-of-home/public/html/editProfile.html", "utf8"));
 });
 
 app.get("/request", function(req, res) {
   res.setHeader("Content-Type", "text/html");
-  res.send(fs.readFileSync("./public/html/request.html", "utf8"));
+  res.send(fs.readFileSync("https://storage.cloud.google.com/taste-of-home/public/html/request.html", "utf8"));
 });
 
 app.get("/requestForm", function(req, res) {
   res.setHeader("Content-Type", "text/html");
-  res.send(fs.readFileSync("./public/html/requestForm.html", "utf8"));
+  res.send(fs.readFileSync("https://storage.cloud.google.com/taste-of-home/public/html/requestForm.html", "utf8"));
 });
 
 
 app.get("/requestSuccess", function(req, res) {
   res.setHeader("Content-Type", "text/html");
-  res.send(fs.readFileSync("./public/html/requestSuccess.html", "utf8"));
+  res.send(fs.readFileSync("https://storage.cloud.google.com/taste-of-home/public/html/requestSuccess.html", "utf8"));
 });
 
 exports.app = functions.https.onRequest(app);
